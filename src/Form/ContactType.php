36f8780058ad770c69ba67144c6172d6cfc8aff9
TONI SALIBA s275112 <s275112@studenti.polito.it>
2020-12-04 15:21:13 +0200

    Fix upload validation

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -3,12 +3,12 @@
 namespace App\Form;
 
 use App\Entity\Contact;
-use PhpParser\Node\Scalar\MagicConst\File;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
 
 class ContactType extends AbstractType
 {
@@ -41,16 +41,16 @@ class ContactType extends AbstractType
 
                 // unmapped fields can't define their validation using annotations
                 // in the associated entity, so you can use the PHP constraint classes
-                // 'constraints' => [
-                //     new File([
-                //         'maxSize' => '1024k',
-                //         'mimeTypes' => [
-                //             'image/jpeg',
-                //             'image/png',
-                //         ],
-                //         'mimeTypesMessage' => 'Please upload a valid PNG/JPEG file',
-                //     ])
-                // ],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '1024k',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid PNG/JPEG file',
+                    ])
+                ],
             ])
             // ...
         ;
